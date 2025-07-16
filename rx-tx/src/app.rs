f15db5f2bdd1724ed366686cbe35e30a6a6be659
vamsi200 <vamsivanama891@gmail.com>
2025-07-16 20:54:26 +0530

    basic ui

--- rx-tx/src/app.rs
+++ rx-tx/src/app.rs
@@ -9,16 +9,24 @@ use ratatui::style::{Color, Modifier, Style, Stylize};
 use ratatui::symbols::scrollbar;
 use ratatui::text::{Line, Masked, Span};
 use ratatui::widgets::block::title;
+use ratatui::widgets::ListState;
 use ratatui::widgets::{Block, Paragraph, Scrollbar, ScrollbarOrientation, ScrollbarState};
 use ratatui::{text::Text, Frame};
 use ratatui::{DefaultTerminal, Terminal};
-use std::collections::HashMap;
+use std::char;
+use std::thread::sleep;
 use std::time::{Duration, Instant};
+use std::vec;
 
 pub struct App {
+    pub mode: Mode,
+    pub selection_state: ListState,
+    pub selected_interface: InterfaceSelected,
     pub prev_stats: Option<Vec<NetworkStats>>,
-    pub graph_data: HashMap<String, Vec<(f64, f64)>>,
+    pub rx_data: Vec<(f64, f64)>,
+    pub tx_data: Vec<(f64, f64)>,
     pub start_time: Instant,
+    pub last_sample_time: f64,
     pub window: [f64; 2],
     pub raw_bytes: bool,
     pub byte_unit: ByteUnit,
@@ -32,9 +40,14 @@ pub struct App {
 impl Default for App {
     fn default() -> Self {
         Self {
+            selection_state: ListState::default(),
+            mode: Mode::Normal,
+            selected_interface: InterfaceSelected::All,
             prev_stats: None,
-            graph_data: HashMap::new(),
+            rx_data: Vec::new(),
+            tx_data: Vec::new(),
             start_time: Instant::now(),
+            last_sample_time: 0.0,
             window: [0.0, 60.0],
             raw_bytes: false,
             byte_unit: ByteUnit::default(),
@@ -54,7 +67,18 @@ pub enum ByteUnit {
     Decimal,
 }
 
-#[derive(Default, Copy, Clone)]
+pub enum Mode {
+    Normal,
+    SelectingInterface { filter: String, index: usize },
+}
+
+#[derive(Clone)]
+pub enum InterfaceSelected {
+    All,
+    Interface(String),
+}
+
+#[derive(Default, Clone, Copy)]
 pub enum Tab {
     #[default]
     Interface,
@@ -71,10 +95,29 @@ impl App {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
         let tick_rate = Duration::from_millis(250);
         let mut last_tick = Instant::now();
+        let interface_name_vec: Vec<String> = parse_proc_net_dev()?
+            .iter()
+            .map(|s| s.name.clone())
+            .collect();
 
         loop {
+            let now = self.start_time.elapsed().as_secs_f64();
+            self.window = [now - 5.0, now];
             let vec_stats = parse_proc_net_dev()?;
+
             let _ = terminal.draw(|frame| self.render(frame, &vec_stats));
+
+            if let Some(prev_data) = &self.prev_stats {
+                for (prev, new) in prev_data.iter().zip(vec_stats.iter()) {
+                    let rx_delta = new.receive.bytes.saturating_sub(prev.receive.bytes);
+                    let tx_delta = new.transmit.bytes.saturating_sub(prev.transmit.bytes);
+
+                    self.rx_data.push((now, rx_delta as f64));
+                    self.tx_data.push((now, tx_delta as f64));
+                }
+            }
+            self.prev_stats = Some(vec_stats);
+
             let timeout = tick_rate.saturating_sub(last_tick.elapsed());
             if !event::poll(timeout)? {
                 last_tick = Instant::now();
@@ -91,6 +134,8 @@ impl App {
                     KeyCode::Char('r') => self.raw_bytes = !self.raw_bytes,
                     KeyCode::Char('d') => self.byte_unit = ByteUnit::Decimal,
                     KeyCode::Char('b') => self.byte_unit = ByteUnit::Binary,
+                    KeyCode::Char('i') => self.current_tab = Tab::Interface,
+                    KeyCode::Char('t') => self.current_tab = Tab::Tcp,
                     _ => {}
                 }
             }
--- rx-tx/src/ui.rs
+++ rx-tx/src/ui.rs
@@ -13,9 +13,14 @@ use ratatui::symbols::scrollbar;
 use ratatui::text::{Line, Masked, Span};
 use ratatui::widgets::block::title;
 use ratatui::widgets::Axis;
+use ratatui::widgets::BorderType;
 use ratatui::widgets::Borders;
 use ratatui::widgets::Chart;
 use ratatui::widgets::Dataset;
+use ratatui::widgets::List;
+use ratatui::widgets::ListItem;
+use ratatui::widgets::Tabs;
+use ratatui::widgets::Widget;
 use ratatui::widgets::{Block, Paragraph, Scrollbar, ScrollbarOrientation, ScrollbarState};
 use ratatui::{text::Text, Frame};
 use ratatui::{DefaultTerminal, Terminal};
@@ -28,19 +33,33 @@ use std::vec;
 pub fn draw_interface_mode(app: &mut App, frame: &mut Frame, data: &Vec<NetworkStats>) {
     let area = frame.area();
 
-    let block = |title: &'static str| Block::bordered().green().title(title.bold());
-    let r_block = |title: &'static str| Block::bordered().blue().title(title.bold());
-    let t_block = |title: &'static str| Block::bordered().red().title(title.bold());
+    let block = |title: &'static str| {
+        Block::bordered()
+            .border_type(BorderType::Rounded)
+            .green()
+            .title(title.bold())
+    };
+    let r_block = |title: &'static str| {
+        Block::bordered()
+            .border_type(BorderType::Rounded)
+            .blue()
+            .title(title.bold())
+    };
+    let t_block = |title: &'static str| {
+        Block::bordered()
+            .border_type(BorderType::Rounded)
+            .red()
+            .title(title.bold())
+    };
 
-    let chunks = Layout::vertical([
-        Constraint::Length(1),
-        Constraint::Percentage(50),
-        Constraint::Percentage(49),
-    ])
-    .split(area);
+    let chunks =
+        Layout::vertical([Constraint::Percentage(50), Constraint::Percentage(50)]).split(area);
 
-    let top_part = chunks[1];
-    let second_part = chunks[2];
+    let top_part = chunks[0];
+
+    let second_part = chunks[1];
+    let split_graph = Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(second_part);
     let top_chunks = Layout::horizontal([
         Constraint::Percentage(25),
         Constraint::Percentage(37),
@@ -50,9 +69,6 @@ pub fn draw_interface_mode(app: &mut App, frame: &mut Frame, data: &Vec<NetworkS
     let interface_rect = top_chunks[0];
     let rx_rect = top_chunks[1];
     let tx_rect = top_chunks[2];
-
-    let title = Line::from("INTERFACE MODE (I)").centered().bold();
-    frame.render_widget(title, chunks[0]);
 
     let para = Paragraph::new(get_network_interfaces(data))
         .white()
@@ -68,7 +84,6 @@ pub fn draw_interface_mode(app: &mut App, frame: &mut Frame, data: &Vec<NetworkS
         .white()
         .scroll((app.vertical_scroll as u16, 0))
         .block(t_block("Transmit"));
-
     frame.render_widget(para, interface_rect);
     frame.render_widget(rx_rectangle, rx_rect);
     frame.render_widget(tx_rectangle, tx_rect);
@@ -80,8 +95,56 @@ pub fn draw_interface_mode(app: &mut App, frame: &mut Frame, data: &Vec<NetworkS
         rx_rect,
         &mut app.horizontal_scroll_state,
     );
+    render_rx_graph(app, frame, split_graph[0]);
+    render_tx_graph(app, frame, split_graph[1]);
 }
 
-pub fn render_graph(app: &mut App, frame: &mut Frame, area: Rect) {
-    todo!()
+pub fn render_rx_graph(app: &mut App, frame: &mut Frame, area: Rect) {
+    let rx_max = app.rx_data.iter().map(|&(_, y)| y).fold(0.0, f64::max);
+
+    let datasets = vec![Dataset::default()
+        .name("RX")
+        .marker(symbols::Marker::Block)
+        .style(Style::default().fg(Color::Blue))
+        .graph_type(ratatui::widgets::GraphType::Bar)
+        .data(&app.rx_data)];
+
+    let chart = Chart::new(datasets)
+        .block(Block::bordered().border_type(BorderType::Rounded))
+        .x_axis(
+            Axis::default()
+                .style(Style::default().fg(Color::Gray))
+                .bounds(app.window),
+        )
+        .y_axis(
+            Axis::default()
+                .style(Style::default().fg(Color::Gray))
+                .bounds([0.0, rx_max]),
+        );
+
+    frame.render_widget(chart, area);
+}
+pub fn render_tx_graph(app: &mut App, frame: &mut Frame, area: Rect) {
+    let tx_max = app.tx_data.iter().map(|&(_, y)| y).fold(0.0, f64::max);
+    let datasets = vec![Dataset::default()
+        .name("TX")
+        .marker(symbols::Marker::Block)
+        .graph_type(ratatui::widgets::GraphType::Bar)
+        .style(Style::default().fg(Color::Red))
+        .data(&app.tx_data)];
+
+    let chart = Chart::new(datasets)
+        .block(Block::bordered().border_type(BorderType::Rounded))
+        .x_axis(
+            Axis::default()
+                .style(Style::default().fg(Color::Gray))
+                .bounds(app.window),
+        )
+        .y_axis(
+            Axis::default()
+                .style(Style::default().fg(Color::Gray))
+                .bounds([0.0, tx_max]),
+        );
+
+    frame.render_widget(chart, area);
 }
